a982fd9cc5723d2079da37ec03e9412a29626f53
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-02-19 18:02:43 +0900

    vimrcとgvimrcのシンボリックリンク等を作成するスクリプトを作成

---
+++ vimfiles/vimrc_install.bat
@@ -0,0 +1,30 @@
+@echo off
+
+NET SESSION > NUL 2>&1
+if not %ERRORLEVEL% == 0 (
+	echo 管理者で実行してください。
+	exit /B 1
+)
+
+rem 現在のカレントを退避
+set CURRENT=%CD%
+
+rem このファイルがあるディレクトリに移動する
+%~d0
+cd %~p0
+
+rem vimrcとgvimrcのシンボリックリンクを%HOME%に作成する
+if not exist %USERPROFILE%\_vimrc (
+	mklink %USERPROFILE%\_vimrc %CD%\vimrc
+)
+if not exist %USERPROFILE%\_gvimrc (
+	mklink %USERPROFILE%\_gvimrc %CD%\gvimrc
+)
+
+rem バックアップディレクトリ等を作成する
+if not exist %USERPROFILE%\.vimbackup (
+	mkdir %USERPROFILE%\.vimbackup
+)
+
+rem 最初のカレントに戻る
+cd %CURRENT%
