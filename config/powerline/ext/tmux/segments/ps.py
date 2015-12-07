c742ecf4366f855770e2e40ad056c2af1526760a
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-12-07 12:10:45 +0900

    tmuxでメモリ使用量の表示を追加

--- config/powerline/ext/tmux/segments/ps.py
+++ config/powerline/ext/tmux/segments/ps.py
@@ -33,3 +33,62 @@ def used_memory_percent(pl, format='{0:.0f}%'):
 		'gradient_level': memory,
 		'highlight_groups': ['used_memory_percent_gradient', 'used_memory_percent'],
 		}]
+
+#'''Return used memory.
+#
+#Requires the ``psutil`` module.
+#
+#:param str format:
+#	Output format. Accepts measured used memory as the first argument.
+#:param str unit:
+#	Output unit (Accepts "MB" or "KB"). Default is "MB"
+#
+#Highlight groups used: ``used_memory``.
+#'''
+def used_memory(pl, format='{0:.0f}', unit='MB'):
+	try:
+		total = psutil.virtual_memory().total
+		available = psutil.virtual_memory().available
+	except:
+		total = psutil.phymem_usage().total
+		available = psutil.phymem_usage().available
+
+	memory = total - available
+	if unit == 'MB':
+		memory = memory / (1024.0 * 1024)
+	elif unit == 'KB':
+		memory = memory / (1024.0)
+
+	return [{
+		'contents': format.format(memory),
+		'gradient_level': memory,
+		'highlight_groups': ['used_memory'],
+		}]
+
+#'''Return total memory.
+#
+#Requires the ``psutil`` module.
+#
+#:param str format:
+#	Output format. Accepts measured used memory as the first argument.
+#:param str unit:
+#	Output unit (Accepts "MB" or "KB"). Default is "MB"
+#
+#Highlight groups used: ``total_memory``.
+#'''
+def total_memory(pl, format='{0:.0f}', unit='MB'):
+	try:
+		memory = psutil.virtual_memory().total
+	except:
+		memory = psutil.phymem_usage().total
+
+	if unit == 'MB':
+		memory = memory / (1024.0 * 1024)
+	elif unit == 'KB':
+		memory = memory / (1024.0)
+
+	return [{
+		'contents': format.format(memory),
+		'gradient_level': memory,
+		'highlight_groups': ['total_memory'],
+		}]
