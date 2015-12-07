50c3b30f51fda3ec97f5466f5af7425aa00250d0
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-12-07 18:32:07 +0900

    powerlineでanzuの結果を表示できるようにした

---
+++ config/powerline/ext/vim/segments/anzu.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# vim:se fenc=utf8 noet ts=8 sw=8 sts=8:
+from __future__ import (unicode_literals, division, absolute_import, print_function)
+
+try:
+	import vim
+except ImportError:
+	vim = object()
+
+import re
+
+from powerline.bindings.vim import (vim_func_exists, vim_get_func)
+
+def search_status(pl):
+	if not vim_func_exists('anzu#search_status'):
+		return []
+
+	status = vim_get_func('anzu#search_status')()
+	if status == '':
+		return []
+
+	return [] if len(status) == 0 else [{
+		'contents': status,
+		'highlight_groups': ['anzu_search_status']
+		}]
