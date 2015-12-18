08acd3eade437997eb557315ebddc0df93f323c8
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-12-18 19:53:47 +0900

    Fugitiveのリビジョン指定で開いたバッファの設定を追加

--- config/powerline/ext/matchers/vim/plugin/fugitive.py
+++ config/powerline/ext/matchers/vim/plugin/fugitive.py
@@ -1,7 +1,22 @@
 # vim:fileencoding=utf-8:noet
 from __future__ import (unicode_literals, division, absolute_import, print_function)
 
-from powerline.bindings.vim import vim_getbufoption
+import re
+
+from powerline.bindings.vim import (vim_getbufoption, buffer_name)
 
 def gitcommit(matcher_info):
 	return vim_getbufoption(matcher_info, 'filetype') == 'gitcommit'
+
+SCHEME_RE = re.compile(b'^\\w[\\w\\d+\\-.]*(?=:)')
+def revision(matcher_info):
+	name = buffer_name(matcher_info)
+	if not name:
+		return False
+
+	scheme = None
+	match = SCHEME_RE.match(name)
+	if match:
+		scheme = match.group(0).decode('ascii')
+
+	return scheme and scheme == 'fugitive'
--- config/powerline/ext/segments/vim/plugin/fugitive.py
+++ config/powerline/ext/segments/vim/plugin/fugitive.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
 # vim:se fenc=utf8 noet ts=8 sw=8 sts=8:
 from __future__ import (unicode_literals, division, absolute_import, print_function)
 
-from powerline.segments.vim import window_cached
+import re
+
+from powerline.theme import requires_segment_info
+from powerline.segments.vim import (window_cached, buffer_name)
 
 try:
 	import vim
@@ -15,4 +19,43 @@ def repos_dir(pl):
 	return [{
 		'contents': dir,
 		'highlight_groups': ['fugitive:repos_dir']
+		}]
+
+FUGITIVE_RE = re.compile(b'^.+\.git//([0-9a-f]+)/(.+)/([^/]+)$')
+
+@requires_segment_info
+def revision(pl, segment_info, length=0):
+	name = buffer_name(segment_info)
+	if not name:
+		return []
+
+	match = FUGITIVE_RE.match(name)
+	if not match:
+		return []
+
+	rev = match.group(1).decode(segment_info['encoding'], 'powerline_vim_strtrans_error')
+	if rev == '0': # TODO 0なら本当にHEADか？3-wayマージのときは1とか2とかある気がするが・・・
+		rev = 'HEAD'
+	elif length > 0 and len(rev) > length:
+		rev = rev[0:length - 1]
+
+	return [{
+		'contents': rev,
+		'highlight_groups': ['fugitive:revision']
+		}]
+
+@requires_segment_info
+def file_directory(pl, segment_info):
+	name = buffer_name(segment_info)
+	if not name:
+		return []
+
+	match = FUGITIVE_RE.match(name)
+	if not match:
+		return []
+
+	directory = match.group(2).decode(segment_info['encoding'], 'powerline_vim_strtrans_error')
+	return [{
+		'contents': directory + '/',
+		'highlight_groups': ['fugitive:file_directory']
 		}]
