a7d619a95242aa26d54f164ddf91e5b0684c42d4
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-12-18 16:54:43 +0900

    VimFilerバッファのステータスライン設定を追加

---
+++ config/powerline/ext/segments/vim/plugin/vimfiler.py
@@ -0,0 +1,16 @@
+# vim:se fenc=utf8 noet ts=8 sw=8 sts=8:
+from __future__ import (unicode_literals, division, absolute_import, print_function)
+
+from powerline.segments.vim import window_cached
+from powerline.bindings.vim import vim_get_func
+
+@window_cached
+def status_string(pl):
+	status_string = vim_get_func('vimfiler#get_status_string')()
+	# if len(status_string) > 0:
+	# 	status_string = status_string.split(" |")[0]
+
+	return [{
+		'contents': status_string,
+		'highlight_groups': ['vimfiler:status_string']
+		}]
