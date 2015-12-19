eb30dd75ac4ac4ac606fd59aaab57b6cabcb8b7f
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2015-12-19 15:46:14 +0900

    Merginal:Branchバッファの表示設定を追加

---
+++ config/powerline/ext/matchers/vim/plugin/merginal.py
@@ -0,0 +1,9 @@
+# vim:fileencoding=utf-8:noet
+from __future__ import (unicode_literals, division, absolute_import, print_function)
+
+import os
+
+from powerline.bindings.vim import (vim_getbufoption, buffer_name)
+
+def branch(matcher_info):
+	return os.path.basename(buffer_name(matcher_info)) == 'Merginal:Branches'
