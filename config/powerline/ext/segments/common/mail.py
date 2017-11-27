fe46dba48ac69bb1e2810e78b7ceaee294c05ffe
Y.Tsutsui <yosuke.tsutsui@gmail.com>
2017-11-27 20:14:02 +0900

    Refactoring powerline segments

--- config/powerline/ext/segments/common/mail.py
+++ config/powerline/ext/segments/common/mail.py
@@ -1,38 +1,46 @@
 # -*- coding: utf-8 -*-
 # vim:se fenc=utf-8 noet:
-from __future__ import (unicode_literals, division, absolute_import, print_function)
+from __future__ import unicode_literals
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import print_function
 
 import json
 import os.path
 
-from imaplib import IMAP4_SSL_PORT, IMAP4_SSL, IMAP4
+from imaplib import IMAP4_SSL_PORT
 
-from powerline.segments.common.mail import (EmailIMAPSegment, _IMAPKey)
+from powerline.segments.common.mail import _IMAPKey
+from powerline.segments.common.mail import EmailIMAPSegment
+
 
 class CustomEmailIMAPSegment(EmailIMAPSegment):
-	@staticmethod
-	def key(config_file=None, server='imap.gmail.com', port=IMAP4_SSL_PORT, folder='INBOX', use_ssl=None, **kwargs):
-		if config_file is None:
-			config_file = 'mail_config.json'
+    @staticmethod
+    def key(config_file=None, server='imap.gmail.com', port=IMAP4_SSL_PORT,
+            folder='INBOX', use_ssl=None, **kwargs):
+        if config_file is None:
+            config_file = 'mail_config.json'
 
-		config_file = os.path.expanduser(config_file)
-		if not os.path.isabs(config_file):
-			config_file = os.path.abspath(os.path.expanduser('~/.config/powerline/' + config_file)) # TODO powerlineのユーザ設定ディレクトリを取得する
+        config_file = os.path.expanduser(config_file)
+        if not os.path.isabs(config_file):
+            # TODO(Y.Tsutsui) powerlineのユーザ設定ディレクトリを取得する
+            config_file = os.path.abspath(
+                os.path.expanduser('~/.config/powerline/' + config_file))
 
-		if not os.path.isfile(config_file):
-			username = ''
-			password = ''
-		else:
-			f = open(config_file)
-			data = json.load(f)
-			f.close
+        if not os.path.isfile(config_file):
+            username = ''
+            password = ''
+        else:
+            f = open(config_file)
+            data = json.load(f)
+            f.close
 
-			username = data['username']
-			password = data['password']
+            username = data['username']
+            password = data['password']
 
-		if use_ssl is None:
-			use_ssl = (port == IMAP4_SSL_PORT)
+        if use_ssl is None:
+            use_ssl = (port == IMAP4_SSL_PORT)
 
-		return _IMAPKey(username, password, server, port, folder, use_ssl)
+        return _IMAPKey(username, password, server, port, folder, use_ssl)
 
 email_imap_alert = CustomEmailIMAPSegment()
--- config/powerline/ext/segments/tmux/ps.py
+++ config/powerline/ext/segments/tmux/ps.py
@@ -1,94 +1,97 @@
 # -*- coding: utf-8 -*-
 # vim:se fenc=utf-8 noet:
 
-from __future__ import (unicode_literals, division, absolute_import, print_function)
+from __future__ import unicode_literals
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import print_function
 
 # import commands
-import multiprocessing
 import psutil
-import re
-import socket
 
-from powerline.lib import add_divider_highlight_group
-from powerline.lib.threaded import ThreadedSegment
-from powerline.segments import with_docstring
 
-#'''Return used memory as a percentage.
+# '''Return used memory as a percentage.
 #
-#Requires the ``psutil`` module.
+# Requires the ``psutil`` module.
 #
-#:param str format:
-#	Output format. Accepts measured used memory as the first argument.
+# :param str format:
+# 	Output format. Accepts measured used memory as the first argument.
 #
-#Highlight groups used: ``used_memory_percent_gradient`` (gradient) or ``used_memory_percent``.
-#'''
+# Highlight groups used: ``used_memory_percent_gradient`` (gradient) or
+# ``used_memory_percent``.
+# '''
 def used_memory_percent(pl, format='{0:.0f}%'):
-	try:
-		memory = psutil.virtual_memory().percent
-	except:
-		memory = psutil.phymem_usage().percent
+    try:
+        memory = psutil.virtual_memory().percent
+    except Exception:
+        memory = psutil.phymem_usage().percent
 
-	return [{
-		'contents': format.format(memory),
-		'gradient_level': memory,
-		'highlight_groups': ['used_memory_percent_gradient', 'used_memory_percent'],
-		}]
+    return [{
+        'contents': format.format(memory),
+        'gradient_level': memory,
+        'highlight_groups': [
+            'used_memory_percent_gradient',
+            'used_memory_percent',
+        ],
+    }]
 
-#'''Return used memory.
+
+# '''Return used memory.
 #
-#Requires the ``psutil`` module.
+# Requires the ``psutil`` module.
 #
-#:param str format:
-#	Output format. Accepts measured used memory as the first argument.
-#:param str unit:
-#	Output unit (Accepts "MB" or "KB"). Default is "MB"
+# :param str format:
+# 	Output format. Accepts measured used memory as the first argument.
+# :param str unit:
+# 	Output unit (Accepts "MB" or "KB"). Default is "MB"
 #
-#Highlight groups used: ``used_memory``.
-#'''
+# Highlight groups used: ``used_memory``.
+# '''
 def used_memory(pl, format='{0:.0f}', unit='MB'):
-	try:
-		total = psutil.virtual_memory().total
-		available = psutil.virtual_memory().available
-	except:
-		total = psutil.phymem_usage().total
-		available = psutil.phymem_usage().available
+    try:
+        total = psutil.virtual_memory().total
+        available = psutil.virtual_memory().available
+    except Exception:
+        total = psutil.phymem_usage().total
+        available = psutil.phymem_usage().available
 
-	memory = total - available
-	if unit == 'MB':
-		memory = memory / (1024.0 * 1024)
-	elif unit == 'KB':
-		memory = memory / (1024.0)
+    memory = total - available
+    if unit == 'MB':
+        memory = memory / (1024.0 * 1024)
+    elif unit == 'KB':
+        memory = memory / (1024.0)
 
-	return [{
-		'contents': format.format(memory),
-		'gradient_level': memory,
-		'highlight_groups': ['used_memory'],
-		}]
+    return [{
+        'contents': format.format(memory),
+        'gradient_level': memory,
+        'highlight_groups': ['used_memory'],
+    }]
 
-#'''Return total memory.
+
+# '''Return total memory.
 #
-#Requires the ``psutil`` module.
+# Requires the ``psutil`` module.
 #
-#:param str format:
-#	Output format. Accepts measured used memory as the first argument.
-#:param str unit:
-#	Output unit (Accepts "MB" or "KB"). Default is "MB"
+# :param str format:
+# 	Output format. Accepts measured used memory as the first argument.
+# :param str unit:
+# 	Output unit (Accepts "MB" or "KB"). Default is "MB"
 #
-#Highlight groups used: ``total_memory``.
-#'''
+# Highlight groups used: ``total_memory``.
+# '''
 def total_memory(pl, format='{0:.0f}', unit='MB'):
-	try:
-		memory = psutil.virtual_memory().total
-	except:
-		memory = psutil.phymem_usage().total
+    try:
+        memory = psutil.virtual_memory().total
+    except Exception:
+        memory = psutil.phymem_usage().total
 
-	if unit == 'MB':
-		memory = memory / (1024.0 * 1024)
-	elif unit == 'KB':
-		memory = memory / (1024.0)
+    if unit == 'MB':
+        memory = memory / (1024.0 * 1024)
+    elif unit == 'KB':
+        memory = memory / (1024.0)
 
-	return [{
-		'contents': format.format(memory),
-		'gradient_level': memory,
-		'highlight_groups': ['total_memory'],
-		}]
+    return [{
+        'contents': format.format(memory),
+        'gradient_level': memory,
+        'highlight_groups': ['total_memory'],
+    }]
